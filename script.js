bf415e2e32c6507a38881734053ecf9d479d153a
max <01091996m@gmail.com>
2022-06-23 20:57:16 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"; // строгий режим
+
+let name = 'joe';
+let i = 0;
+while (i < 2) {
+    console.log('happy');
+    i = i + 1;
+}
+
+console.log('happy ' + name);
+console.log('happy');
